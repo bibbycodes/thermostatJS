c690fe1f1c77ce8e90222ebed603d18e3d16282f
Robert <bibbycodes@gmail.com>
2019-11-13 15:35:31 +0000

    has minTemp, can decrease temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,18 @@ describe("Thermostat", function() {
   it("should have a starting temperature of 20", function() {
     expect(thermo.temperature).toEqual(20)
   })
+
+  it("can increase the temperature", function() {
+    thermo.up(5)
+    expect(thermo.temperature).toEqual(25)
+  })
+
+  it("can decrease the temperature", function() {
+    thermo.down(5)
+    expect(thermo.temperature).toEqual(15)
+  })
+
+  it("has a minimum temperature", function() {
+    expect(thermo.minTemp).toEqual(10)
+  })
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,16 @@
 function Thermostat() {
   this.temperature = 20
+  this.minTemp = 10
 }
 
 // Thermostat.prototype.temperature = function() {
 //   return this.temperature 
 // }
+
+Thermostat.prototype.up = function (amount) {
+  this.temperature += amount
+}
+
+Thermostat.prototype.down = function (amount) {
+  this.temperature -= amount
+}
