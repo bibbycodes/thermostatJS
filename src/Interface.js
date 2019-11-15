c29d59f9166ebad9187ac2c58bcc08a4f0c3698b
Robert <bibbycodes@gmail.com>
2019-11-15 16:56:12 +0000

    added weather api

--- src/Interface.js
+++ src/Interface.js
@@ -4,9 +4,23 @@
     $('.usage-indicator').attr('id', t.energyUsage());
   }
 
+  function showWeather(city) {
+    api_key = "appid=a3d9eb01d4de82b9b8d0849ef604dbed"
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&${api_key}&units=metric`, function(data) {
+      temp = data.main.temp
+      weather = data.weather[0].main
+      $('#city').text(city)
+      $('.weather-temp').text(temp)
+      $('.weather').text(weather)
+      console.log(data);
+      })  
+    }
+
 $(document).ready(function() {
+  showWeather("Los Angeles")
   t = new Thermostat()
   updateTemperature()
+
   $('#power-saving').text(`Power Saving: ${t.returnValForPowerSaving()}`)
 
   $('#plus').on('click', () => {
@@ -27,11 +41,19 @@ $(document).ready(function() {
 
   $('#ps-mode').on('click', () => {
     t.togglePowerSaving()
+
     if (t.isPowerSavingOn) {
-      $('.ps-mode').attr('src', "src/svg/008-energy-1.svg")
-    } else {
       $('.ps-mode').attr('src', "src/svg/009-saving.svg")
+    } else {
+      $('.ps-mode').attr('src', "src/svg/008-energy-1.svg")
     }
     updateTemperature()
+    })
+
+    $('#submit').on('click', function(event) {
+      event.preventDefault()
+      city = $('#select-city').val()
+      console.log(city)
+      showWeather(city)
+    })
   })
-})
